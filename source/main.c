40f7f554df8785970c17b56d6dbb1bd899fe4d90
PEGA5Y5 <yashraj1822@yahoo.com>
2020-03-17 14:27:02 +0530

    Welcome window with instructions added

--- source/main.c
+++ source/main.c
@@ -11,53 +11,35 @@ static const int height = 0;
 int main(int argc, char **argv)
 {
     // Initialize SDL video and audio systems
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+    SDL_Init(SDL_INIT_AUDIO);
 
     // Initialize SDL mixer
     Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
 
-
+    if(Mix_Init(MIX_INIT_MOD) != MIX_INIT_MOD)
+    {
+    	printf("Error");
+    	return -1;
+    }
+	
 
     Mix_Chunk *A = Mix_LoadWAV("audio/alphabets/A.wav");
     Mix_Chunk *B = Mix_LoadWAV("audio/alphabets/B.wav");
-    SDL_Window *window = SDL_CreateWindow("Hello, SDL2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
 
-    // Create a renderer (accelerated and in sync with the display refresh rate)
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 
 
     Mix_PlayChannel(-1, A, 0);
-    delay(1000);
-
+    SDL_Delay(1000);
+    
     char a;
     printf("Enter a: ");
-    scanf("%d",&a);
+    scanf("%c",&a);
     printf("%c %d...",a,a);
     Mix_PlayChannel(-1, A, 0);
-    delay(1000);
+    SDL_Delay(1000);
 
     Mix_PlayChannel(-1, B, 0);
-    delay(1000);
-
-
-
-
-
-        // Clear screen
-        SDL_RenderClear(renderer);
-
-        // Draw
-
-        // Show what was drawn
-        SDL_RenderPresent(renderer);
-    //}
-
-    // Release resources
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    //Mix_FreeMusic(backgroundSound);
+    SDL_Delay(1000);
 
     Mix_CloseAudio();
     SDL_Quit();
