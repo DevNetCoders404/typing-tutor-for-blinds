3ded4e3ae86bfefd5175a9fdf7d661e96d4472e3
PEGA5Y5 <yashraj1822@yahoo.com>
2020-03-23 22:11:43 +0530

    word exercise added

---
+++ src/audio.h
@@ -0,0 +1,260 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_mixer.h>
+void play_alphabet(char c)
+{
+    Mix_Chunk *A = Mix_LoadWAV("audio/alphabets/A.wav");
+    Mix_Chunk *B = Mix_LoadWAV("audio/alphabets/B.wav");
+    Mix_Chunk *C = Mix_LoadWAV("audio/alphabets/C.wav");
+    Mix_Chunk *D = Mix_LoadWAV("audio/alphabets/D.wav");
+    Mix_Chunk *E = Mix_LoadWAV("audio/alphabets/E.wav");
+    Mix_Chunk *F = Mix_LoadWAV("audio/alphabets/F.wav");
+    Mix_Chunk *G = Mix_LoadWAV("audio/alphabets/G.wav");
+    Mix_Chunk *H = Mix_LoadWAV("audio/alphabets/H.wav");
+    Mix_Chunk *I = Mix_LoadWAV("audio/alphabets/I.wav");
+    Mix_Chunk *J = Mix_LoadWAV("audio/alphabets/J.wav");
+    Mix_Chunk *K = Mix_LoadWAV("audio/alphabets/K.wav");
+    Mix_Chunk *L = Mix_LoadWAV("audio/alphabets/L.wav");
+    Mix_Chunk *M = Mix_LoadWAV("audio/alphabets/M.wav");
+    Mix_Chunk *N = Mix_LoadWAV("audio/alphabets/N.wav");
+    Mix_Chunk *O = Mix_LoadWAV("audio/alphabets/O.wav");
+    Mix_Chunk *P = Mix_LoadWAV("audio/alphabets/P.wav");
+    Mix_Chunk *Q = Mix_LoadWAV("audio/alphabets/Q.wav");
+    Mix_Chunk *R = Mix_LoadWAV("audio/alphabets/R.wav");
+    Mix_Chunk *S = Mix_LoadWAV("audio/alphabets/S.wav");
+    Mix_Chunk *T = Mix_LoadWAV("audio/alphabets/T.wav");
+    Mix_Chunk *U = Mix_LoadWAV("audio/alphabets/U.wav");
+    Mix_Chunk *V = Mix_LoadWAV("audio/alphabets/V.wav");
+    Mix_Chunk *W = Mix_LoadWAV("audio/alphabets/W.wav");
+    Mix_Chunk *X = Mix_LoadWAV("audio/alphabets/X.wav");
+    Mix_Chunk *Y = Mix_LoadWAV("audio/alphabets/Y.wav");
+    Mix_Chunk *Z = Mix_LoadWAV("audio/alphabets/Zedd.wav");
+     switch(c)
+        {
+        case 'a':
+                Mix_PlayChannel(-1, A, 0);
+                SDL_Delay(1000);
+                break;
+        case 'b':
+                Mix_PlayChannel(-1, B, 0);
+                SDL_Delay(1000);
+                break;
+        case 'c':
+                Mix_PlayChannel(-1, C, 0);
+                SDL_Delay(1000);
+                break;
+        case 'd':
+                Mix_PlayChannel(-1, D, 0);
+                SDL_Delay(1000);
+                break;
+        case 'e':
+                Mix_PlayChannel(-1, E, 0);
+                SDL_Delay(1000);
+                break;
+        case 'f':
+                Mix_PlayChannel(-1, F, 0);
+                SDL_Delay(1000);
+                break;
+
+        case 'g':
+                Mix_PlayChannel(-1, G, 0);
+                SDL_Delay(1000);
+                break;
+        case 'h':
+                Mix_PlayChannel(-1, H, 0);
+                SDL_Delay(1000);
+                break;
+        case 'i':
+                Mix_PlayChannel(-1, I, 0);
+                SDL_Delay(1000);
+                break;
+        case 'j':
+                Mix_PlayChannel(-1, J, 0);
+                SDL_Delay(1000);
+                break;
+        case 'k':
+                Mix_PlayChannel(-1, K, 0);
+                SDL_Delay(1000);
+                break;
+        case 'l':
+                Mix_PlayChannel(-1, L, 0);
+                SDL_Delay(1000);
+                break;
+        case 'm':
+                Mix_PlayChannel(-1, M, 0);
+                SDL_Delay(1000);
+                break;
+        case 'n':
+                Mix_PlayChannel(-1, N, 0);
+                SDL_Delay(1000);
+                break;
+        case 'o':
+                Mix_PlayChannel(-1, O, 0);
+                SDL_Delay(1000);
+                break;
+        case 'p':
+                Mix_PlayChannel(-1, P, 0);
+                SDL_Delay(1000);
+                break;
+        case 'q':
+                Mix_PlayChannel(-1, Q, 0);
+                SDL_Delay(1000);
+                break;
+        case 'r':
+                Mix_PlayChannel(-1, R, 0);
+                SDL_Delay(1000);
+                break;
+        case 's':
+                Mix_PlayChannel(-1, S, 0);
+                SDL_Delay(1000);
+                break;
+        case 't':
+                Mix_PlayChannel(-1, T, 0);
+                SDL_Delay(1000);
+                break;
+        case 'u':
+                Mix_PlayChannel(-1, U, 0);
+                SDL_Delay(1000);
+                break;
+        case 'v':
+                Mix_PlayChannel(-1, V, 0);
+                SDL_Delay(1000);
+                break;
+        case 'w':
+                Mix_PlayChannel(-1, W, 0);
+                SDL_Delay(1000);
+                break;
+        case 'x':
+                Mix_PlayChannel(-1, X, 0);
+                SDL_Delay(1000);
+                break;
+        case 'y':
+                Mix_PlayChannel(-1, Y, 0);
+                SDL_Delay(1000);
+                break;
+        case 'z':
+                Mix_PlayChannel(-1, Z, 0);
+                SDL_Delay(1000);
+                break;
+        default:
+                break;
+        }
+}
+void play_words(int i)
+{
+    Mix_Chunk *A = Mix_LoadWAV("audio/words/After...'A' 'F' 'T' 'E' 'R'.wav");
+    Mix_Chunk *B = Mix_LoadWAV("audio/words/Alomst. 'A' 'L' 'M' 'O' 'S' 'T'.wav");
+    Mix_Chunk *C = Mix_LoadWAV("audio/words/Because. 'B' 'E' 'C' 'A' 'U' 'S' 'E'.wav");
+    Mix_Chunk *D = Mix_LoadWAV("audio/words/Between. 'B' 'E' 'T' 'W' 'E' 'E' 'N'.wav");
+    Mix_Chunk *E = Mix_LoadWAV("audio/words/Later. 'L' 'A' 'T' 'E' 'R'.wav");
+    Mix_Chunk *F = Mix_LoadWAV("audio/words/Might. 'M' 'I' 'G' 'H' 'T' .wav");
+    Mix_Chunk *G = Mix_LoadWAV("audio/words/Mother. 'M' 'O' 'T' 'H' 'E' 'R'.wav");
+    Mix_Chunk *H = Mix_LoadWAV("audio/words/Never. 'N' 'E' 'V' 'E' 'R'.wav");
+    Mix_Chunk *I = Mix_LoadWAV("audio/words/Paper. 'P' 'A' 'P' 'E' 'R'.wav");
+    Mix_Chunk *J = Mix_LoadWAV("audio/words/Really 'R' 'E' 'A' 'L' 'L' 'Y'.wav");
+    Mix_Chunk *K = Mix_LoadWAV("audio/words/River. 'R' 'I' 'V' 'E' 'R'.wav");
+    Mix_Chunk *L = Mix_LoadWAV("audio/words/Sometimes. 'S' 'O' 'M' 'E' 'T' 'I' 'M' 'E' 'S'.wav");
+    Mix_Chunk *M = Mix_LoadWAV("audio/words/Together. 'T' 'O' 'G' 'E' 'T' 'H' 'E' 'R'.wav");
+    Mix_Chunk *N = Mix_LoadWAV("audio/words/Watch. 'W' 'A' 'T' 'C' 'H'.wav");
+    Mix_Chunk *O = Mix_LoadWAV("audio/words/World. 'W' 'O' 'R' 'L' 'D'.wav");
+    switch(i)
+    {
+    case 1:
+            Mix_PlayChannel(-1, A, 0);
+            SDL_Delay(5000);
+            break;
+    case 2:
+            Mix_PlayChannel(-1, B, 0);
+            SDL_Delay(5000);
+            break;
+    case 3:
+            Mix_PlayChannel(-1, C, 0);
+            SDL_Delay(5000);
+            break;
+    case 4:
+            Mix_PlayChannel(-1, D, 0);
+            SDL_Delay(5000);
+            break;
+    case 5:
+            Mix_PlayChannel(-1, E, 0);
+            SDL_Delay(5000);
+            break;
+    case 6:
+            Mix_PlayChannel(-1, F, 0);
+            SDL_Delay(5000);
+            break;
+    case 7:
+            Mix_PlayChannel(-1, G, 0);
+            SDL_Delay(5000);
+            break;
+    case 8:
+            Mix_PlayChannel(-1, H, 0);
+            SDL_Delay(5000);
+            break;
+    case 9:
+            Mix_PlayChannel(-1, I, 0);
+            SDL_Delay(5000);
+            break;
+    case 10:
+            Mix_PlayChannel(-1, J, 0);
+            SDL_Delay(5000);
+            break;
+    case 11:
+            Mix_PlayChannel(-1, K, 0);
+            SDL_Delay(5000);
+            break;
+    case 12:
+            Mix_PlayChannel(-1, L, 0);
+            SDL_Delay(5000);
+            break;
+    case 13:
+            Mix_PlayChannel(-1, M, 0);
+            SDL_Delay(5000);
+            break;
+    case 14:
+            Mix_PlayChannel(-1, N, 0);
+            SDL_Delay(5000);
+            break;
+    case 15:
+            Mix_PlayChannel(-1, O, 0);
+            SDL_Delay(5000);
+            break;
+    default:
+            break;
+    }
+}
+void play_sentences(int i)
+{
+    Mix_Chunk *A = Mix_LoadWAV("audio/sentences/All questions asked by five watched experts amaze the judge..wav");
+    Mix_Chunk *B = Mix_LoadWAV("audio/sentences/The five boxing wizards jump quickly..wav");
+    Mix_Chunk *C = Mix_LoadWAV("audio/sentences/The job requires extra pluck and zeal from every young wage earner.wav");
+    Mix_Chunk *D = Mix_LoadWAV("audio/sentences/The quick brown fox jumps over the lazy dog.wav");
+    Mix_Chunk *E = Mix_LoadWAV("audio/sentences/Woven silk pyjamas exchanged for blue quartz.wav");
+    switch(i)
+    {
+    case 1:
+            Mix_PlayChannel(-1, A, 0);
+            SDL_Delay(4500);
+            break;
+    case 2:
+            Mix_PlayChannel(-1, B, 0);
+            SDL_Delay(3500);
+            break;
+    case 3:
+            Mix_PlayChannel(-1, C, 0);
+            SDL_Delay(4500);
+            break;
+    case 4:
+            Mix_PlayChannel(-1, D, 0);
+            SDL_Delay(3500);
+            break;
+    case 5:
+            Mix_PlayChannel(-1, E, 0);
+            SDL_Delay(3500);
+            break;
+    default:
+            break;
+    }
+}
+
--- src/welwin.c
+++ src/welwin.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <ncurses.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include "audio.h"
 
 #define WIDTH 30
 #define HEIGHT 10 
@@ -10,11 +15,14 @@ int starty = 0;
 char *choices[] = { 
 			"1. Alphabet Exercise",
 			"2. Word Exercise",
-			"3. Sentence Exercisess",
+			"3. Sentence Exercise",
 			"4. Exit",
 		  };
 int n_choices = sizeof(choices) / sizeof(char *);
 void print_menu(WINDOW *menu_win, int highlight);
+void alpha_exercise();
+void word_exercise();
+void sentence_exercise();
 
 int main()
 {	WINDOW *menu_win;
@@ -26,6 +34,8 @@ int main()
 	clear();
 	noecho();
 	cbreak();	
+	SDL_Init(SDL_INIT_AUDIO);
+    	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
 	startx = (80 - WIDTH) / 2;
 	starty = (24 - HEIGHT) / 2;
 	mvprintw(1, 13, "  _      ________   _________  __  _______  __________ ");
@@ -44,7 +54,9 @@ int main()
 
 	WINDOW *ins_win = newwin(11, 70, 13, (80 - 70) / 2);
 	box(ins_win, 0, 0);
+	wattron(ins_win,A_BOLD);
 	mvwprintw(ins_win,1,26,"** INSTRUCTION **");
+	wattroff(ins_win,A_BOLD);
  	mvwprintw(ins_win,3,3,"1. Select any Exercise from Alphabet, Word, Sentence.");
 	mvwprintw(ins_win,5,3,"2. To hear Audible instructions on screen, Press 'TAB'.");
 	mvwprintw(ins_win,7,3,"3. For each exercise, Timer is provided.");
@@ -61,6 +73,7 @@ int main()
 	print_menu(menu_win, highlight);
 	while(1)
 	{	c = wgetch(menu_win);
+		choice=0;			
 		switch(c)
 		{	case KEY_UP:
 				if(highlight == 1)
@@ -81,16 +94,250 @@ int main()
 				refresh();
 				break;
 		}
+		if(choice >0 && choice < 4)	/* User did a choice come out of the infinite loop */
+		{
+			switch(choice)
+			{
+				case 1:
+					alpha_exercise();
+					break;
+				case 2:
+					word_exercise();
+					break;
+				case 3:
+				//	sentence_exercise();
+					break;
+				default:
+					break;
+			}	
+		
+		}
+		clear();
+		mvprintw(0, 0, "Use arrow keys to go up and down, Press enter to select a choice");
+		refresh();
 		print_menu(menu_win, highlight);
-		if(choice != 0)	/* User did a choice come out of the infinite loop */
+		if(choice == 4)
 			break;
+		
 	}	
-	//mvprintw(23, 0, "You chose choice %d with choice string %s\n", choice, choices[choice - 1]);
+	//mvprintw(23, 0, "You chose choice %d with choice string %s key pressed=%d\n", choice, choices[choice - 1],c);
+	//getch();
+	Mix_CloseAudio();
+   	SDL_Quit();
 	clrtoeol();
 	refresh();
 	endwin();
 	return 0;
 }
+
+void alpha_exercise()
+{
+	clear();
+	refresh();
+	int i=0,j=1,err=0;
+	char chr,ans;
+	WINDOW *disp_win = newwin(10, 70, 1, (80 - 70) / 2);
+	box(disp_win, 0, 0);
+	mvwprintw(disp_win, 1, 26, "** INSTRUCTIONS **");
+	mvwprintw(disp_win, 2, 1, "1. Enter each character displayed or sound produced.");
+	mvwprintw(disp_win, 3, 1, "2. Timer for this exercise is 2 minutes(120 seconds).");
+	mvwprintw(disp_win, 4, 1, "3. If input is wrong, enter character again.");
+	mvwprintw(disp_win, 5, 1, "4. For correct input character, Next character will be generated -");
+	mvwprintw(disp_win, 6, 1, "   until time expires.");
+	wattron(disp_win, A_REVERSE);
+	mvwprintw(disp_win, 7, 24, "PRESS ANY KEY TO START");
+	mvwprintw(disp_win, 8, 22, "PRESS ESC TO PREVIOUS MENU");
+	wattroff(disp_win, A_REVERSE);
+	wrefresh(disp_win);
+
+	attron(A_BOLD);
+	mvprintw(13, 31, "[[[ INPUT BOX ]]]");
+	attroff(A_BOLD);
+
+	WINDOW *input_win = newwin(5, 30, 15, (80 - 30) / 2);
+	box(input_win, 0, 0);
+	wrefresh(input_win);
+	move(8, 51);
+	refresh();
+	ans = getch();
+	if(ans == 27)
+	{
+		return;
+	}	
+	while(i!=10)
+	{
+		srand(time(NULL));
+		chr = (rand() % (122 - 97 + 1)) + 97;
+		resume:
+		wclear(disp_win);
+		box(disp_win, 0, 0);
+		wattron(disp_win, A_BOLD);
+		mvwprintw(disp_win, 1, 1, "CHARACTER GENERATED:           %c ",chr);
+		play_alphabet(chr);
+		wrefresh(disp_win);
+		move(16, 26+(i));
+		ans = getch();
+		while(chr != ans)
+		{
+			int pause;
+			if(ans == 27)
+			{
+				wattron(disp_win, A_REVERSE);
+				mvwprintw(disp_win, 6, 28, "** PAUSE MENU **");
+				mvwprintw(disp_win, 7, 24, "PRESS ANY KEY TO START");
+				mvwprintw(disp_win, 8, 21, "PRESS ENTER/ESC TO END EXERCISE");
+				wattroff(disp_win, A_REVERSE);
+				wrefresh(disp_win);
+				pause = getch();
+				if(pause == 10 || pause == 27)
+					return;
+				else
+					goto resume;
+			}
+			else if(ans == 9)
+			{
+				play_alphabet(chr);
+				ans = getch();
+			}
+			else
+			{
+				wclear(disp_win);
+				box(disp_win, 0, 0);
+				mvwprintw(disp_win, 1, 1, "CHARACTER GENERATED:           %c           TRY AGAIN",chr);
+				play_alphabet(chr);
+				wrefresh(disp_win);
+				move(16, 26+(i));
+				ans = getch();
+				err++;	
+			}
+			
+		}
+		mvwprintw(input_win, j, i+1,"%c",ans);
+		wrefresh(input_win);
+		i++;
+	}
+	wclear(disp_win);
+	box(disp_win, 0, 0);
+	wattron(disp_win, A_REVERSE);
+	mvwprintw(disp_win, 1, 1, "EXERCISE COMPLETED...",chr);
+	wattron(disp_win, A_BOLD);
+	mvwprintw(disp_win, 2, 1, "FINAL SCORE: CORRECT-%d WRONG-%d",i,err);
+	wrefresh(disp_win);
+	getch();
+	clear;
+}
+
+void word_exercise()
+{
+	char wrd[15][10] = {"AFTER ", "ALMOST ", "BECAUSE ", "BETWEEN ", "LATER ", "MIGHT ", "MOTHER ", "NEVER ", "PAPER ", "REALLY ", "RIVER ", "SOMETIMES ", "TOGETHER ", "WATCH ", "WORLD "};
+	clear();
+	refresh();
+	int i=0,j=1,err=0,r=0,c=0;
+	char chr='a',ans,chr2;
+	WINDOW *disp_win = newwin(10, 70, 1, (80 - 70) / 2);
+	box(disp_win, 0, 0);
+	mvwprintw(disp_win, 1, 26, "** INSTRUCTIONS **");
+	mvwprintw(disp_win, 2, 1, "1. Enter each string displayed or sound produced.");
+	mvwprintw(disp_win, 3, 1, "2. Timer for this exercise is 2 minutes(120 seconds).");
+	mvwprintw(disp_win, 4, 1, "3. If input is wrong, enter string again.");
+	mvwprintw(disp_win, 5, 1, "4. For correct input string, Next string will be generated -");
+	mvwprintw(disp_win, 6, 1, "   until time expires.");
+	wattron(disp_win, A_REVERSE);
+	mvwprintw(disp_win, 7, 24, "PRESS ANY KEY TO START");
+	mvwprintw(disp_win, 8, 22, "PRESS ESC TO PREVIOUS MENU");
+	wattroff(disp_win, A_REVERSE);
+	wrefresh(disp_win);
+
+	attron(A_BOLD);
+	mvprintw(13, 31, "[[[ INPUT BOX ]]]");
+	attroff(A_BOLD);
+
+	WINDOW *input_win = newwin(5, 30, 15, (80 - 30) / 2);
+	box(input_win, 0, 0);
+	wrefresh(input_win);
+	move(8, 51);
+	refresh();
+	ans = getch();
+	if(ans == 27)
+	{
+		return;
+	}	
+	while(i!=10)
+	{
+		srand(time(NULL));
+		r = (rand() % (14 - 0 + 1)) + 0;
+		resume:
+		wclear(disp_win);
+		box(disp_win, 0, 0);
+		wattron(disp_win, A_BOLD);
+		mvwprintw(disp_win, 1, 1, "STRING GENERATED:           %s ",wrd[r]);
+		play_words(r+1);
+		wrefresh(disp_win);
+		move(16, 26+(i));
+		c=0;
+		chr2 = wrd[r][c+1];
+		while(chr2 != ' ')
+		{
+			chr = wrd[r][c];
+			chr2 = wrd[r][c+1];
+			ans = getch();
+			while(chr != ans || chr != (ans+32))
+			{
+				int pause;
+				if(ans == 27)
+				{
+					wattron(disp_win, A_REVERSE);
+					mvwprintw(disp_win, 6, 28, "** PAUSE MENU **");
+					mvwprintw(disp_win, 7, 24, "PRESS ANY KEY TO START");
+					mvwprintw(disp_win, 8, 21, "PRESS ENTER/ESC TO END EXERCISE");
+					wattroff(disp_win, A_REVERSE);
+					wrefresh(disp_win);
+					pause = getch();
+					if(pause == 10 || pause == 27)
+						return;
+					else
+						goto resume;
+				}
+				else if(ans == 9)
+				{
+					play_words(r+1);
+					ans = getch();
+				}
+				else
+				{
+					wclear(disp_win);
+					box(disp_win, 0, 0);
+					mvwprintw(disp_win, 1, 1, "STRING GENERATED:           %s         TRY AGAIN",wrd[r]);
+					//play_words(r+1);
+					wrefresh(disp_win);
+					move(16, 26+(i));
+					ans = getch();
+					err++;	
+				}
+				
+			}
+			mvwprintw(input_win, j, c+1,"%c",ans);
+			wrefresh(input_win);
+			c++;
+		}
+		wclear(input_win);
+		box(input_win, 0, 0);
+		wrefresh(input_win);
+		move(8, 51);
+		refresh();
+		i++;
+	}
+	wclear(disp_win);
+	box(disp_win, 0, 0);
+	wattron(disp_win, A_REVERSE);
+	mvwprintw(disp_win, 1, 1, "EXERCISE COMPLETED...",chr);
+	wattron(disp_win, A_BOLD);
+	mvwprintw(disp_win, 2, 1, "FINAL SCORE: CORRECT-%d WRONG-%d",i,err);
+	wrefresh(disp_win);
+	getch();
+	clear;
+}
+	
 
 
 void print_menu(WINDOW *menu_win, int highlight)
